dbd7a9967319d1bcd7b3f9cfdf436e4dd6764164
Lai Chun Pong <laichunpongben@gmail.com>
2017-05-02 17:45:32 +0800

    Handle path

--- db.py
+++ db.py
@@ -1,15 +1,16 @@
 import os
 import sqlite3
 
-def init_tables():
-    tables = [f[:-4] for f in sorted(os.listdir(os.path.dirname(os.path.abspath(__file__)))) if f.endswith('.txt')]
 
-    conn = sqlite3.connect('ttc_schedules.db')
+def init_tables():
+    folder_path = os.path.dirname(os.path.abspath(__file__))
+    tables = [f[:-4] for f in sorted(os.listdir(folder_path)) if f.endswith('.txt')]
+
+    conn = sqlite3.connect(os.path.join(folder_path, 'ttc_schedules.db'))
     with conn:
         for table in tables:
-            with open('{}.txt'.format(table), 'r') as table_txt:
+            with open(os.path.join(folder_path, '{}.txt'.format(table)), 'r') as table_txt:
                 lines = table_txt.read().splitlines()
-
 
             conn.execute("DROP TABLE IF EXISTS {0}".format(table))
             conn.execute("CREATE TABLE {0} ({1})".format(table, lines[0]))
